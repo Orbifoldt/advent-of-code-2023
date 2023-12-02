0607e97d688c6a275e163ed3540dea016afb4743
Orbifoldt <eikenrode02@gmail.com>
2023-12-02 14:05:26 +0100

    Solving part 1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 use std::fs;
 
-fn main() {
-    println!("Hello, world!");
+use regex::Regex;
 
+fn main() {
     let contents = fs::read_to_string("./input_1.txt")
         .expect("Should be able to read the file");
-    println!("{contents}")
+
+    println!("part1: Sum of numbers={}", part_1(contents))
+}
+
+fn part_1(contents: String) -> i64 {
+    let mut sum = 0;
+    let regex = Regex::new(r"(\d)").unwrap();
+
+    for line in contents.lines() {
+        let digits: Vec<String> = regex.find_iter(line)
+            .map(|d| d.as_str().to_string())
+            .collect();
+
+        let number: i64 = format!("{}{}", digits[0], digits.last().unwrap())
+            .parse().unwrap();
+        sum += number;
+    }
+    sum
 }
