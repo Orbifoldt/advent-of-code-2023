4087ba305f5793f9f48e6640408494d7d49e4483
Orbifoldt <eikenrode02@gmail.com>
2023-12-03 16:10:13 +0100

    Init day 3

---
+++ src/day03.rs
@@ -0,0 +1,10 @@
+pub fn main() {
+    println!("day03")
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn should_() {}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-pub mod day01;
+mod day01;
 mod day02;
+mod day03;
 
 fn main() {
-    // day01::main();
+    day01::main();
     day02::main();
+    day03::main();
 }
 
