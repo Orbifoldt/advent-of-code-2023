77325b19e4931cc31c79acace8ad441dd45fd7e2
Orbifoldt <eikenrode02@gmail.com>
2023-12-17 16:28:05 +0100

    Solving Day 17 part 2: bootleg-Dijkstra

--- src/day17.rs
+++ src/day17.rs
@@ -1,30 +1,28 @@
 use std::{fs, vec};
 use std::collections::{HashMap, HashSet};
-use std::sync::{Arc, Mutex};
 
 use itertools::Itertools;
 use crate::common::{Direction, next_coord};
 use crate::common::Direction::{East, North, South, West};
-use rayon::prelude::*;
 
 pub fn main() {
     let input = &fs::read_to_string("./inputs/day17/input.txt").unwrap();
     part1(input);
-    // part2(input);
+    part2(input);
 }
 
-// 1586 too high in 1558 steps
-// 1202 ??? in ~260 steps
-// 1149 in 314 steps
-// not tried yet: 1128 in 318 steps
 fn part1(input: &str) -> usize {
     let field = parse(input);
-    // println!("{:?}", field);
-    bfs_with_cache_find_path(&field) as usize
+    let min_heat = bfs_with_cache_find_path(&field, 1, 3);
+    println!("Part 1: minimum heat is {min_heat}");
+    min_heat
 }
 
 fn part2(input: &str) -> usize {
-    todo!()
+    let field = parse(input);
+    let min_heat = bfs_with_cache_find_path(&field, 4, 10);
+    println!("Part 2: minimum heat is {min_heat}");
+    min_heat
 }
 
 fn parse(input: &str) -> Vec<Vec<u16>> {
@@ -35,20 +33,21 @@ fn parse(input: &str) -> Vec<Vec<u16>> {
         .collect()
 }
 
-fn bfs_with_cache_find_path(field: &Vec<Vec<u16>>) -> usize {
+fn bfs_with_cache_find_path(field: &Vec<Vec<u16>>, min_straight: u8, max_straight: u8) -> usize {
     let (width, height) = (field[0].len() as u16, field.len() as u16);
     let start = State { x: 0, y: 0, accumulated_heat: 0, total_straight: 0, last_direction: South };
-    let upper_bound = upper_bound_heat(field);
-    println!("Finding lowest heat path on field of size {width}x{height}, with upper bound {upper_bound}");
+    let start2 = State { x: 0, y: 0, accumulated_heat: 0, total_straight: 0, last_direction: East };
+    let upper_bound = if min_straight <= 1 { upper_bound_heat(field) } else { usize::MAX };
+    println!("Finding lowest heat path on field of size {width}x{height} with upperbound {upper_bound}");
 
     let mut cache: HashMap<(u16, u16, Direction, u8), usize> = HashMap::new();
 
-    let mut states = vec![start];
+    let mut states = vec![start, start2];
     let mut min_heat_incurred = upper_bound;
     let mut steps = 0;
 
     while !states.is_empty() {
-        println!("\nRunning cycle {steps}, with {} states:", states.len());
+        // println!("\nRunning cycle {steps}, with {} states:", states.len());
         let mut new_states = HashSet::new();
         let mut end_states = vec![];
         states.iter()
@@ -56,24 +55,41 @@ fn bfs_with_cache_find_path(field: &Vec<Vec<u16>>) -> usize {
                 if state.reached_end(width, height) {
                     end_states.push(state);
                 } else {
-                    Direction::iterator()
-                        .filter(|&direction| direction != &state.last_direction.inverse())
-                        .for_each(|&direction| {
-                            if let Some((x, y)) = next_coord((state.x, state.y), direction, (width, height)) {
-                                let total_straight = if direction == state.last_direction { state.total_straight + 1 } else { 0 };
-                                let accumulated_heat = state.accumulated_heat + (field[y as usize][x as usize] as usize);
-                                if total_straight < 3 && accumulated_heat <= min_heat_incurred {
-                                    let key = (x, y, direction, total_straight);
-                                    let state = State { x, y, accumulated_heat, total_straight, last_direction: direction };
-                                    if let Some(h) = cache.get_mut(&key) {
-                                        if *h > accumulated_heat {
-                                            *h = accumulated_heat;
-                                            new_states.insert(state);
-                                        }
-                                    } else {
-                                        cache.insert(key, accumulated_heat);
+                    let dirs = if state.total_straight < min_straight {
+                        vec![&state.last_direction]
+                    } else {
+                        Direction::iterator()
+                            .filter(|&direction| direction != &state.last_direction.inverse())
+                            .collect()
+                    };
+
+                    dirs.iter()
+                        .filter_map(|&dir| next_coord((state.x, state.y), *dir, (width, height)).map(|coord| (dir, coord)))
+                        .for_each(|(&direction, (x, y))| {
+                            let min_straight = min_straight as u16;
+                            let can_move_min_distance = (direction == state.last_direction) || match direction {
+                                North => state.y >= min_straight,
+                                East => state.x + min_straight <= width - 1,
+                                South => state.y + min_straight <= height - 1,
+                                West => state.x >= min_straight,
+                            };
+                            let total_straight = if direction == state.last_direction { state.total_straight + 1 } else { 1 };
+                            let accumulated_heat = state.accumulated_heat + (field[y as usize][x as usize] as usize);
+
+                            if can_move_min_distance
+                                && total_straight <= max_straight
+                                && accumulated_heat <= min_heat_incurred
+                            {
+                                let key = (x, y, direction, total_straight);
+                                let state = State { x, y, accumulated_heat, total_straight, last_direction: direction };
+                                if let Some(h) = cache.get_mut(&key) {
+                                    if *h > accumulated_heat {
+                                        *h = accumulated_heat;
                                         new_states.insert(state);
                                     }
+                                } else {
+                                    cache.insert(key, accumulated_heat);
+                                    new_states.insert(state);
                                 }
                             }
                         });
@@ -86,9 +102,7 @@ fn bfs_with_cache_find_path(field: &Vec<Vec<u16>>) -> usize {
             }
         }
 
-        states = new_states.iter().map(|state| state.clone()).collect();
-        states.sort_by_key(|state| (state.accumulated_heat as isize) - (state.distance_from_start().pow(2) as isize));
-
+        states = new_states.into_iter().collect();
         steps += 1;
     }
     println!("Reached end with min heat {min_heat_incurred}");
@@ -147,7 +161,7 @@ impl State {
 
 #[cfg(test)]
 mod tests {
-    use crate::day17::{parse, part1, upper_bound_heat};
+    use crate::day17::{parse, part1, part2, upper_bound_heat};
 
     #[test]
     fn part_1_dummy_example() {
@@ -245,5 +259,33 @@ mod tests {
 2546548887735
 4322674655533";
         assert_eq!(upper_bound_heat(&parse(input)), 134)
+    }
+
+    #[test]
+    fn part_2_example() {
+        let input = r"2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533";
+        assert_eq!(part2(input), 94)
+    }
+
+    #[test]
+    fn part_2_example2() {
+        let input = r"111111111111
+999999999991
+999999999991
+999999999991
+999999999991";
+        assert_eq!(part2(input), 71)
     }
 }
