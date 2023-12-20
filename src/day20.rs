8cc7d5f8cd780ec7fa0e757d6921841d0b207391
Orbifoldt <eikenrode02@gmail.com>
2023-12-20 20:20:14 +0100

    Day 20 part 1 complete! Fixing a bug of having no memory #relatable

--- src/day20.rs
+++ src/day20.rs
@@ -1,46 +1,21 @@
 use std::{fs, vec};
-use std::cell::RefCell;
-use std::collections::{HashMap, HashSet, VecDeque};
-use std::convert::identity;
+use std::collections::{HashMap, VecDeque};
 
 use itertools::Itertools;
-use ModuleType::{Button, Conjunction, FlipFlop, Output};
-use crate::common::{Direction, next_coord};
-use crate::common::Direction::{East, North, South, West};
-use crate::day20::ModuleType::Broadcaster;
+
+use ModuleType::{Broadcaster, Button, Conjunction, FlipFlop, Output};
 
 pub fn main() {
-    // let a = Module { label: "button".to_string(), module_type: Button, connected_to: vec!["broadcaster".to_string()], state: false };
-    // let b = Module { label: "broadcaster".to_string(), module_type: Button, connected_to: vec![], state: false };
-    // let mut map: HashMap<String, Box<Module>> = HashMap::new();
-    // map.insert("button".to_string(), Box::new(a));
-    // map.insert("broadcaster".to_string(), Box::new(b));
-    //
-    // map.entry("button".to_string()).and_modify(|a| mutate(a.as_mut(), &mut map));
-
     let input = &fs::read_to_string("./inputs/day20/input.txt").unwrap();
     part1(input);
 // part2(input);
 }
 
-// fn mutate(a: &mut Module, map: &mut HashMap<String, Module>){
-//     a.connected_to.iter().for_each(|to| {
-//         let target = map.get_mut(to).unwrap();
-//         mutate(target, map)
-//     });
-// }
-
-
 fn part1(input: &str) -> usize {
-    // let instructions = parse_pt1(input);
-    // // Take the naive flood-fill approach
-    // let dug_squares = count_interior_squares(&instructions);
-    // println!("Part 1: dug {dug_squares} squares");
-    // dug_squares
     let (mut connections, mut modules) = parse(input);
     let mut counter = (0, 0);
     for _ in 0..1000 {
-        send_signal(&"".to_string(), &"button".to_string(), false, &mut connections, &mut modules, (&mut counter.0, &mut counter.1));
+        send_signal_bfs(&mut connections, &mut modules, (&mut counter.0, &mut counter.1));
     }
     let product = counter.0*counter.1;
     println!("Total sent {} low and {} high signals, product is {}", counter.0, counter.1, product);
@@ -57,6 +32,12 @@ struct Module {
     module_type: ModuleType,
     state: bool,
     memory: HashMap<String, bool>
+}
+
+impl Module {
+    pub fn new(label: String, module_type: ModuleType) -> Self {
+        Self { label, module_type, state: false, memory: HashMap::new() }
+    }
 }
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
@@ -77,8 +58,9 @@ fn parse(input: &str) -> (HashMap<String, Vec<String>>, HashMap<String, Module>)
             _ => Broadcaster
         };
         let label = if module_type == Broadcaster { a.to_string() } else { a[1..].to_string() };
-        (label.clone(), Module { label, module_type, state: false, memory: HashMap::new()})
+        (label.clone(), Module::new(label, module_type))
     }).collect::<HashMap<String, Module>>();
+
     let mut connections_map = input.lines().map(|line| {
         let (a, b) = line.split(" -> ").tuple_windows().next().unwrap();
         let label = if a == "broadcaster" { a.to_string() } else { a[1..].to_string() };
@@ -86,8 +68,17 @@ fn parse(input: &str) -> (HashMap<String, Vec<String>>, HashMap<String, Module>)
         (label.clone(), connected_to_string)
     }).collect::<HashMap<String, Vec<String>>>();
 
-    module_map.insert("button".to_string(), Module { label: "button".to_string(), module_type: Button, state: false, memory: HashMap::new() });
+    module_map.insert("button".to_string(), Module::new("button".to_string(),  Button));
     connections_map.insert("button".to_string(), vec!["broadcaster".to_string()]);
+
+    // Conjunctions need to know who are connected to them
+    module_map.iter_mut().filter(|(label, module)| module.module_type == Conjunction)
+        .for_each(|(label, conjunction)| {
+            conjunction.memory = connections_map.iter().filter(|&(_, connections)| connections.contains(label))
+                .map(|(other_label, _)| (other_label.clone(), false))
+                .collect::<HashMap<String, bool>>();
+        });
+
     (connections_map, module_map)
 }
 
@@ -96,57 +87,53 @@ fn send_signal_bfs(map: &HashMap<String, Vec<String>>, modules: &mut HashMap<Str
     let broadcast = &"broadcaster".to_string();
     let mut signals_to_send = VecDeque::from([(button, broadcast, false)]);
 
-    while let Some((from_name, target_name, high_signal)) = signals_to_send.pop_front() {
+    while let Some((from_name, current_name, high_signal)) = signals_to_send.pop_front() {
         if high_signal {
             *high += 1
         } else {
             *low += 1
         }
         let string = if high_signal { "high" } else {"low"};
-        println!("{from_name} -{string}-> {target_name}");
+        // println!("{from_name} -{string}-> {current_name}");
 
-        if let Some(target) = modules.get_mut(target_name) {
-            // print!("{:?}: ", target.module_type);
-            match target.module_type {
-                Output => {
-                    // println!("Received {high_signal} signal from {from_name} to output {:?}", target_name)
-                }
-                Button => {
-                    // println!("Sending {high_signal} signal to broadcaster");
-                    for next_node in &map[target_name] {
-                        signals_to_send.push_back((target_name, next_node, high_signal));
-                    }
-                }
+        let signal_to_send;
+        if let Some(current) = modules.get_mut(current_name) {
+            // print!("  > {current_name} ({:?}): ", current.module_type);
+            match current.module_type {
+                Button => unreachable!(),
                 Broadcaster => {
-                    // println!("Sending {high_signal} signal to all connectors {:?}", map[target_name]);
-                    for next_node in &map[target_name] {
-                        signals_to_send.push_back((target_name, next_node, high_signal));
-                    }
+                    // println!("Sending {high_signal} signal to all connectors {:?}", map[current_name]);
+                    signal_to_send = high_signal;
                 }
                 FlipFlop => {
-                    // println!("Received {high_signal} signal from '{from_name}' at flip-flop '{}'", target_name);
-                    if high_signal {
-                        // ignore
-                    } else {
+                    // println!("Received {high_signal} signal from '{from_name}'");
+                    if !high_signal {
                         // flip
-                        let new_state = !target.state;
-                        target.state = new_state;
-
-                        for next_node in &map[target_name] {
-                            signals_to_send.push_back((target_name, next_node, new_state));
-                        }
+                        let new_state = !current.state;
+                        current.state = new_state;
+                        signal_to_send = new_state;
+                    } else {
+                        continue;
                     }
                 }
                 Conjunction => {
-                    // println!("Received {high_signal} signal from '{from_name}' at conjunction '{target_name}'");
-                    let memory_entry = target.memory.entry(from_name.clone()).or_insert(high_signal);
+                    // println!("Received {high_signal} signal from '{from_name}'");
+                    // println!("    Memory contains:");
+                    // current.memory.iter().for_each(|(k,v)| println!("      {k}: {v}"));
+                    let memory_entry = current.memory.entry(from_name.clone()).or_insert(high_signal);
                     *memory_entry = high_signal;
+                    // println!("      *{from_name}: {high_signal}*");
                     // if all high send low, else send high
-                    let signal_to_send = !target.memory.values().all(|&b| b); // memory is guaranteed non-empty
-                    for next_node in &map[target_name] {
-                        signals_to_send.push_back((target_name, next_node, signal_to_send));
-                    }
+                    signal_to_send = !current.memory.values().all(|&b| b); // memory is guaranteed non-empty
                 }
+                Output => {
+                    // println!("Received {high_signal} signal from {from_name}");
+                    continue
+                }
+            }
+            for next_node in &map[current_name] {
+                signals_to_send.push_back((current_name, next_node, signal_to_send));
+                // println!("    - Next turn sending from {current_name} signal {signal_to_send} to {next_node}");
             }
         }
     }
@@ -155,6 +142,8 @@ fn send_signal_bfs(map: &HashMap<String, Vec<String>>, modules: &mut HashMap<Str
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
     use crate::day20::part1;
 
     #[test]
@@ -176,4 +165,11 @@ mod tests {
 &con -> output";
         assert_eq!(part1(input), 11687500)
     }
+
+    #[test]
+    fn part_1_input() {
+        let input = &fs::read_to_string("./inputs/day20/input.txt").unwrap();
+        assert_eq!(part1(input), 666795063)
+    }
+
 }
